de1fcd7ee12c16819a7c0a47a5143380a8b02b96
Zilow <zilow1234@gmail.com>
2018-11-11 11:33:25 +0100

    added shield skill to project

---
+++ Scripts/Spells/Spell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spells {
+
+public enum SpellType {
+	PROJECTAIL,
+	AURA,
+}
+public class Spell : MonoBehaviour {
+
+
+	public SpellType spell_type;
+}
+} //namespace spells;
